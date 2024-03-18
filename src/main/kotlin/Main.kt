3bb23f1b31901c9694ae4263c56c1c3748d9478f
Geb101 <georgestrel@gmail.com>
2024-03-18 23:00:01 +0300

    Remove token

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,6 @@ import dev.kord.core.Kord
 
 class Main {
     suspend fun main() {
-        val token = Kord("OTAwMzM4MDAxNDU5MTUwOTE4.G0d3ba.VWx7kf0ZPxhKcsn-t72wXiB8UqxFHKxzoARYGc")
+        val token = Kord("")
     }
 }
